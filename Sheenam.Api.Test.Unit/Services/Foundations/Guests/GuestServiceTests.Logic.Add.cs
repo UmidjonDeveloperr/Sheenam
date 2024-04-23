524b2c8bf0437e5d55a2b0cd2bc4fbf48e6cef6d
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-04-23 19:38:55 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Test.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenam.Api.Models.Foundation.Guests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sheenam.Api.Test.Unit.Services.Foundations.Guests
+{
+	public partial class GuestServiceTests
+	{
+		[Fact]
+		public async Task ShouldAddGuestAsync()
+		{
+			// given
+			Guest randomGuest = CreateRandomGuest();
+			Guest inputGuest = randomGuest;
+			Guest storageGuest = inputGuest;
+			Guest expectedGuest = storageGuest.DeepClone();
+
+			this.storageBrokerMock.Setup(broker =>
+			  broker.InsertGuestAsync(inputGuest))
+				.ReturnsAsync(expectedGuest);
+
+			// when 
+			Guest actualGuest =
+				await this.guestService.AddGuestAsync(inputGuest);
+
+			// then
+			actualGuest.Should().BeEquivalentTo(expectedGuest);
+
+			this.storageBrokerMock.Verify(broker =>
+			  broker.InsertGuestAsync(It.IsAny<Guest>()), Times.Once());
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
---
+++ Sheenam.Api.Test.Unit/Services/Foundations/Guests/GuestServiceTests.Validation.Add.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit;
+using Sheenam.Api.Models.Foundation.Guests;
+using Sheenam.Api.Models.Foundation.Guests.Exceptions;
+using Moq;
+
+namespace Sheenam.Api.Test.Unit.Services.Foundations.Guests
+{
+	public partial class GuestServiceTests
+	{
+		[Fact]
+		public async Task ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync()
+		{
+			// given
+			Guest nullGuest = null;
+			NullGuestException nullGuestException = new();
+
+			GuestValidationException expectedGuestValidationException =
+			new(nullGuestException);
+
+			// when
+			ValueTask<Guest> addGuestTask =
+				this.guestService.AddGuestAsync(nullGuest);
+
+			// then
+			await Assert.ThrowsAsync<GuestValidationException>(() =>
+			  addGuestTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedGuestValidationException))),
+				Times.Once());
+
+			this.storageBrokerMock.Verify(broker =>
+			 broker.InsertGuestAsync(It.IsAny<Guest>()), Times.Never);
+
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
